3fca08452977e4d964aec5bd79337219a05822b5
Nhu.Che <quynhnhu243@gmail.com>
2022-11-11 15:25:10 +0700

    đẩy source java basic bai tap lên github

--- src/dieukienif/ToanTuDieuKien.java
+++
@@ -1,9 +0,0 @@
-package dieukienif;
-
-public class ToanTuDieuKien {
-    public static void main(String[] args) {
-        int a = 8;
-        int b = 10;
-        System.out.println((a > b)?"A lớn hơn B" : "a nhỏ hơn b");
-    }
-}
